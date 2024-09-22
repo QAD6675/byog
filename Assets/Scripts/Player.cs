caa2820ed8ec13e4948792c6b5dcc61a05814f53
QAD6675 <qadirixx01@gmail.com>
2024-09-22 02:01:21 +0100

    todays work almost everything

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,20 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
+    Animator animator;
     [SerializeField]Vector3 startingPosition;
+    [SerializeField]GameObject panel;
     private Transform transform;
     private bool hasDbjump = false;
+    private bool touchingWall = false;
     private float horizontal;
     private bool dashing = false;
     private bool isFacingRight = true;
-    private float speed = 8f;
-    private float jumpingPower = 6f;
-    private float dashPower = 10f;
+    [SerializeField]private float speed = 8f;
+    [SerializeField]private float jumpingPower = 12f;
+    [SerializeField]private float dashPower = 10f;
     private bool onGround = false;
-    private bool touchingWall = false;
-    bool[] abilities={false,//walk left
+    [SerializeField]bool[] abilities={false,//walk left
     false,//jump
     false,//dash
     false,//double jump
@@ -27,6 +30,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+     animator=GetComponent<Animator>();
      rb=GetComponent<Rigidbody2D>();
      transform=GetComponent<Transform>();
     }
@@ -37,6 +41,7 @@ public class Player : MonoBehaviour
     if (dashing)return;//disable input while dashing
 
     horizontal = Input.GetAxisRaw("Horizontal");
+    animator.SetBool("walking",horizontal!=0&&onGround);
 
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
@@ -47,25 +52,33 @@ public class Player : MonoBehaviour
             transform.localScale = localScale;
         }
     if (!abilities[1])return;//exit if you didn't unlock that
-    if (Input.GetButtonDown("Jump") && (onGround||hasDbjump))
+    if ((Input.GetButtonDown("Jump")||Input.GetKey(KeyCode.W)) && (onGround||hasDbjump))
     {
         rb.velocity = new (rb.velocity.x, jumpingPower);
         if (!onGround){//consume dbjump
             hasDbjump=false;
             return;
         }
+        animator.SetBool("jumping",true);
+        animator.SetBool("walking",false);
         onGround = false;
     }
     if(!abilities[2])return;//exit if you didn't unlock that
-    if (Input.GetKey("Shift"))
+    if (Input.GetKey(KeyCode.S))
     {
         dash();
+    }
+    if(!abilities[4])return;//exit if you didn't unlock that
+    if (Input.GetKey(KeyCode.P))
+    {
+        pause();
     }
     }
     private void OnCollisionEnter2D(Collision2D other) {
         switch (other.gameObject.tag) {
             case "ground":
                 onGround = true;
+                animator.SetBool("jumping",false);
                 if (abilities[3]) hasDbjump = true;//recharge dbjump
                 break;
             case "wall":
@@ -76,21 +89,27 @@ public class Player : MonoBehaviour
                 break;
             case "left":
                 unlock(0);
+                Destroy(other.gameObject);
                 break;
             case "jump":
                 unlock(1);
+                Destroy(other.gameObject);
                 break;
             case "dash":
                 unlock(2);
+                Destroy(other.gameObject);
                 break;
             case "dbjump":
                 unlock(3);
+                Destroy(other.gameObject);
                 break;
             case "ui.pause":
                 unlock(4);
+                Destroy(other.gameObject);
                 break;
             case "ui.creds":
                 unlock(5);
+                Destroy(other.gameObject);
                 break;
             default:
                 break;
@@ -122,5 +141,10 @@ public class Player : MonoBehaviour
     }
     private void unlock(int i){
         abilities[i]=true;
+    }
+    private void pause(){
+        Time.timeScale=0;
+        if (!abilities[5])return;
+        panel.GetComponent<panelScript>().lockCreds();
     }
 }
---
+++ Assets/Scripts/panelScript.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+public class panelScript : MonoBehaviour {
+    public Button playButton;
+    public Button menuButton;
+    public Button credsButton;
+
+    void Start()
+    {
+        playButton.onClick.AddListener(PlayGame);
+        credsButton.onClick.AddListener(OpenCreds);
+        menuButton.onClick.AddListener(OpenMenu);
+    }
+    public void PlayGame()
+    {
+        Time.timeScale=1;
+    }
+    public void lockCreds(){
+        credsButton.enabled=false;
+    }
+    public void OpenMenu()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+    public void OpenCreds()
+    {
+        SceneManager.LoadScene("creds");
+    }
+}
